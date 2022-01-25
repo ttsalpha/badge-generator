f348c198c56c3278bff2a9a07a83781f049f4a81
Son Tran <ttsalpha@icloud.com>
2022-01-26 00:08:59 +0700

    Update homepage

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 <div align="center">
     <img src="public/logo.png" width="128" height="128" alt=""/>
     <h1>Badge Generator</h1>
-    <p>Create a badge can be included in GitHub README or any other web page.</p>
+    <p>Generate Markdown badges for Github README</p>
 </div>
 
 ## Example
@@ -15,13 +15,13 @@
 ## Query
 
 ```javascript
-const badge = {
+const example = {
     label: 'npm',         // [Text]
     status: 'v1.2.3',     // [Text]
-    color: 'blue',        // [Color RGB] or [Color Name] (default: 'blue')
+    color: 'teal',        // [Color RGB] or [Color Name] (default: 'blue')
     icon: 'npm',          // [Text] Use icon name in SimpleIcons.org
-    iconColor: 'eeeeee',  // [Color RGB] or [Color Name] (default: '959da5')
-    labelColor: '444'     // [Color RGB] or [Color Name] (default: '333333')
+    labelColor: '444',    // [Color RGB] or [Color Name] (default: '333333')
+    iconColor: 'e4e4e4'   // [Color RGB] or [Color Name] (default: '959da5')
 }
 ```
 
@@ -45,7 +45,7 @@ Repo: [@ttsalpha/badge-generator][repo-url]
 
 [e1]: https://badge-generator.vercel.app/api?icon=github&label=test&status=passing&color=green
 
-[e2]: https://badge-generator.vercel.app/api?icon=npm&label=package&status=v1.3.2&color=teal
+[e2]: https://badge-generator.vercel.app/api?icon=npm&label=package&status=v1.2.3&color=teal
 
 [e3]: https://badge-generator.vercel.app/api?label=license&status=GPL-3.0&color=pink
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,8 @@
 import type {NextPage} from 'next'
 import Head from 'next/head'
-import Image from 'next/image'
 import {useEffect, useState} from 'react'
 import ReactGA from 'react-ga'
-import Site from './site'
+import Site from '../components/site'
 import styles from '../styles/Home.module.css'
 import logo from '/public/logo.png'
 
@@ -17,7 +16,7 @@ const Home: NextPage = () => {
     })
 
     const [display, setDisplay] = useState({
-        result: 'none',
+        result: false,
         advanced: false
     })
 
@@ -34,7 +33,7 @@ const Home: NextPage = () => {
 
     const exampleList = [
         'https://badge-generator.vercel.app/api?icon=github&label=test&status=passing&color=green',
-        'https://badge-generator.vercel.app/api?icon=npm&label=package&status=v1.3.2&color=teal',
+        'https://badge-generator.vercel.app/api?icon=npm&label=package&status=v1.2.3&color=teal',
         'https://badge-generator.vercel.app/api?label=license&status=GPL-3.0&color=pink',
         'https://badge-generator.vercel.app/api?icon=twitter&label=account&status=ttsalpha&color=1DA1F2&iconColor=1DA1F2',
         'https://badge-generator.vercel.app/api?icon=typescript&iconColor=white&label=TypeScript&labelColor=blue'
@@ -42,8 +41,7 @@ const Home: NextPage = () => {
 
     const getExample = () => {
         return exampleList.map((e) => (
-            /* eslint-disable */
-            <img key={e.toString()} style={{margin: 2}} alt="" src={e}/>
+            <img key={e} className={styles.example} alt="" src={e}/>
         ))
     }
 
@@ -55,17 +53,28 @@ const Home: NextPage = () => {
             + (badge.labelColor ? `&labelColor=${badge.labelColor}` : '')
             + (badge.iconColor ? `&iconColor=${badge.iconColor}` : '')
         const result = "https://badge-generator.vercel.app/api?" + query.substring(1)
-        setBadge({...badge, src: result, alt: badge.label})
-        setDisplay({...display, result: 'block'})
+        setBadge({...badge, src: result, alt: badge.label || badge.status || badge.icon})
+        setDisplay({...display, result: true})
     }
 
-    const displayAdvanced = () => {
-        setDisplay({...display, advanced: !display.advanced})
-    }
-
-    function handleEnter(event: any) {
+    const handleEnter = (event: any) => {
         if (event.key === 'Enter')
             displayResult()
+    }
+
+    const defaultColors: string[] = [
+        'red', 'orange', 'yellow', 'green', 'mint',
+        'teal', 'cyan', 'blue', 'purple', 'pink',
+        'gray', 'light', 'dark', 'black', 'white'
+    ]
+
+    const displayDefaultColors = (item: string) => {
+        return defaultColors.map((e) => (
+            <li key={item + e} className={styles.option}
+                onClick={() => setBadge({...badge, [item]: e})}>
+                {e}
+            </li>
+        ))
     }
 
     return (
@@ -84,50 +93,62 @@ const Home: NextPage = () => {
                     </a>
                     <h3>by Son Tran @ttsalpha</h3>
                 </div>
-                <div className={styles.logo}>
-                    <Image
-                        src={logo}
-                        width={250} height={250}
-                        alt="Logo"
-                        layout="fixed"
-                        objectFit="cover"
-                        quality={100}/>
-                </div>
+                <img src={logo.src} alt='Logo' className={styles.logo}/>
                 <div className={styles.guide}>
                     <h3>Example</h3>
                     {getExample()}
-                    <p>Create a badge can be included in GitHub README or any other web page.</p>
+                    <p>Generate Markdown badges for Github README.</p>
                     <h3>Generator</h3>
-                    <div>
-                        <input className={styles.input} placeholder={'label'}
-                               onChange={e => setBadge({...badge, label: e.target.value})}/>
-                        <input className={styles.input} placeholder={'status'}
-                               onChange={e => setBadge({...badge, status: e.target.value})}/>
-                        <input className={styles.input} placeholder={'color'}
-                               onChange={e => setBadge({...badge, color: e.target.value})}/>
-                        <input className={styles.input} placeholder={'icon'}
-                               onChange={e => setBadge({...badge, icon: e.target.value})}/>
+                    <div className={styles.field}>
+                        <div>
+                            <input className={styles.input} placeholder={'label'}
+                                   onChange={e => setBadge({...badge, label: e.target.value})}/>
+                            <input className={styles.input} placeholder={'status'}
+                                   onChange={e => setBadge({...badge, status: e.target.value})}/>
+                            <div className={styles.colorStatus}>
+                                <ul className={`${styles.datalist} ${styles.suggestColorStatus}`}>
+                                    {displayDefaultColors('color')}</ul>
+                                <input className={styles.input} placeholder={'color'}
+                                       value={badge.color}
+                                       onChange={e => setBadge({...badge, color: e.target.value})}/>
+                            </div>
+                            <input className={styles.input} placeholder={'icon'}
+                                   onChange={e => setBadge({...badge, icon: e.target.value})}/>
+                            <div>
+                                <p className={styles.advanced} onClick={() =>
+                                    setDisplay({...display, advanced: !display.advanced})}>
+                                    Advanced Query</p>
+                                <div style={{display: display.advanced ? 'initial' : 'none'}}>
+                                    <div className={styles.colorLabel}>
+                                        <ul className={`${styles.datalist} ${styles.suggestColorLabel}`}>
+                                            {displayDefaultColors('labelColor')}</ul>
+                                        <input className={styles.input} placeholder={'labelColor'}
+                                               value={badge.labelColor}
+                                               onChange={e => setBadge({...badge, labelColor: e.target.value})}/>
+                                    </div>
+                                    <div className={styles.colorIcon}>
+                                        <ul className={`${styles.datalist} ${styles.suggestColorIcon}`}>
+                                            {displayDefaultColors('iconColor')}</ul>
+                                        <input className={styles.input} placeholder={'iconColor'}
+                                               value={badge.iconColor}
+                                               onChange={e => setBadge({...badge, iconColor: e.target.value})}/>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                         <button className={styles.generator} onClick={displayResult}>Generator</button>
                     </div>
-                    <div>
-                        <p className={styles.advanced} onClick={displayAdvanced}>Advanced Query</p>
-                        <div style={{display: display.advanced ? 'inline-block' : 'none'}}>
-                            <input className={styles.input} placeholder={'labelColor'}
-                                   onChange={e => setBadge({...badge, labelColor: e.target.value})}/>
-                            <input className={styles.input} placeholder={'iconColor'}
-                                   onChange={e => setBadge({...badge, iconColor: e.target.value})}/>
-                        </div>
-                    </div>
-                    <div style={{display: display.result}}>
+                    <div style={{display: display.result ? 'initial' : 'none'}}>
                         <div className={styles.result}>New badge:&nbsp;
-                            {/* eslint-disable */}
                             <img src={badge.src} alt={badge.alt}/>
                         </div>
                         <div className={styles.result}>Markdown:&nbsp;
-                            <div className={styles.preview}>![{badge.alt}]({badge.src})</div>
-                            <button className={styles.copy} onClick={() => {
-                                navigator.clipboard.writeText(`![${badge.alt}](${badge.src})`).then(null)
-                            }}>Copy
+                            <div className={styles.preview}>[![{badge.alt}]({badge.src})](https://example.com)</div>
+                            <button className={styles.copy} onClick={() =>
+                                navigator.clipboard.writeText(
+                                    `[![${badge.alt}](${badge.src})](https://example.com)`
+                                ).then(null)
+                            }>Copy
                             </button>
                         </div>
                     </div>
@@ -150,12 +171,12 @@ const Home: NextPage = () => {
                             [Text] Use icon name in SimpleIcons.org
                         </div>
                         <div>
-                            <div className={styles.key}>iconColor: &apos;&apos;</div>
-                            [Color RGB] or [Color Name] (default: &apos;959da5&apos;)
-                        </div>
-                        <div>
                             <div className={styles.key}>labelColor: &apos;444&apos;</div>
                             [Color RGB] or [Color Name] (default: &apos;333333&apos;)
+                        </div>
+                        <div>
+                            <div className={styles.key}>iconColor: &apos;e4e4e4&apos;</div>
+                            [Color RGB] or [Color Name] (default: &apos;959da5&apos;)
                         </div>
                     </div>
                     <h3>Meta</h3>
